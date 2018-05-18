9e7a78b22ce5ba7e3c9f75bb3f3d055c576dd8f2
liwei <weili@thoughtworks.com>
2018-05-18 13:06:34 +0800

    u

--- level05/src/FirstApp/AppM.hs
+++ level05/src/FirstApp/AppM.hs
@@ -2,12 +2,19 @@
 module FirstApp.AppM (liftEither, AppM) where
 
 import FirstApp.Types (Error)
+import Control.Applicative (liftA2)
 
 newtype AppM a = AppM (IO (Either Error a)) deriving Functor
 
 instance Applicative AppM where
   pure = AppM . pure . pure
-  (AppM iea) <*> (AppM ief) = AppM (iea <*> ief)
+  (AppM iea) <*> (AppM ief) = AppM (liftA2 (<*>) iea ief)
+
+instance Monad AppM where
+  return = pure
+  (AppM iea) >>= f = AppM $ iea >>= \ea -> case ea of
+                                             Left a -> return $ Left a
+                                             Right x -> f x
 
 liftEither :: Either Error a -> AppM a
 liftEither = AppM . return
